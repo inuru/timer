83ab8f48ac3c1b1ba5642290445b5e18d011798c
Zainul Abidin <a.zain.abidin@gmail.com>
2026-05-28 09:58:36 +0700

    fix manifest and icon

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'capsa-timer-v1';
+const CACHE = 'capsa-timer-v2';
 const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  'https://fonts.googleapis.com/css2?family=Share+Tech+Mono&family=Bebas+Neue&display=swap'
+  '/timer/index.html',
+  '/timer/manifest.json',
+  '/timer/icon-192.png',
+  '/timer/icon-512.png',
+  '/timer/favicon.ico'
 ];
 
 self.addEventListener('install', e => {
@@ -23,6 +23,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/timer/index.html')))
   );
 });
